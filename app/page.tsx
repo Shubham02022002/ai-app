2c7a39b31c7cb6c5e064293656766917c4612dab
Shubham02022002 <chaudhary2001shubham@gmail.com>
2025-05-25 14:27:08 +0530

    + Home

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,53 @@
+import CompanionCard from '@/components/CompanionCard'
+import CompanionList from '@/components/CompanionList'
+import CTA from '@/components/CTA'
 import { Button } from '@/components/ui/button'
+import { recentSessions } from '@/constants'
 import React from 'react'
 
 const Page = () => {
   return (
-    <div>
-      <h1 className='text-2xl underline'>Welcome to my Saas.</h1>
-      <Button>Let's get started</Button>
-    </div>
+    <main>
+      <h1>Popular Companions</h1>
+
+      <section className='home-section'>
+        <CompanionCard
+          id="123"
+          name="xyz"
+          topic="xyx"
+          subject="Science"
+          duration={45}
+          color="#ff3d21"
+        />
+        <CompanionCard
+          id="123"
+          name="xyz"
+          topic="xyx"
+          subject="Science"
+          duration={45}
+          color="#ff3d21"
+        />
+        <CompanionCard
+          id="123"
+          name="xyz"
+          topic="xyx"
+          subject="Science"
+          duration={45}
+          color="#ff3d21"
+        />
+      </section>
+
+      <section className='home-section'>
+        <CompanionList
+          title="Recently completed sessions"
+          companions={recentSessions}
+          classNames="w-2/3 max-lg:w-full"
+        />
+        <CTA />
+      </section>
+
+
+    </main>
   )
 }
 
