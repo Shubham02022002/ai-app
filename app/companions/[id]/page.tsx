b7f99cd556911e922a64a392f56f9cff7565af46
Shubham02022002 <chaudhary2001shubham@gmail.com>
2025-05-26 20:39:57 +0530

    + Ai Convo, Sentry

--- app/companions/[id]/page.tsx
+++ app/companions/[id]/page.tsx
@@ -1,3 +1,4 @@
+import CompanionComponent from "@/components/CompanionComponent";
 import { getCompanion } from "@/lib/actions/companion.actions";
 import { getSubjectColor } from "@/lib/utils";
 import { currentUser } from "@clerk/nextjs/server";
@@ -11,8 +12,9 @@ interface CompanionSessionPageProps {
 
 const CompanionSession = async ({ params }: CompanionSessionPageProps) => {
   const { id } = await params;
-  const { name, subject, title, topic, duration } = await getCompanion(id);
+  const companion = await getCompanion(id);
   const user = await currentUser();
+  const { name, subject, title, topic, duration } = companion;
   if (!user) redirect('/sign-in');
   if (!name) redirect('/companions')
   return (
@@ -40,6 +42,9 @@ const CompanionSession = async ({ params }: CompanionSessionPageProps) => {
           {duration} minutes
         </div>
       </article>
+      <CompanionComponent {...companion} companionId={id}
+        userName={user.firstName!}
+        userImage={user.imageUrl!} />
     </main>
   )
 }
